d773fb1fbe4722ff9a5ee8b2d3f084cab8bfdb4f
MadhumethaJeyaganesan <67951541+MadhumethaJeyaganesan@users.noreply.github.com>
2021-08-22 13:04:50 +0530

    Update var.c

--- 3_Implementation/var.c
+++ 3_Implementation/var.c
@@ -9,12 +9,14 @@
 #define MAX_SIZE_USER_NAME 30
 #define MAX_SIZE_PASSWORD  20
 #define FILE_NAME  "winterfell.bin"
+
 // Macro related to the books info
 #define MAX_BOOK_NAME   50
 #define MAX_AUTHOR_NAME 50
 #define MAX_STUDENT_NAME 50
 #define MAX_STUDENT_ADDRESS 300
 #define FILE_HEADER_SIZE  sizeof(sFileHeader)
+
 //structure to store date
 typedef struct
 {
@@ -22,11 +24,13 @@ typedef struct
     int mm;
     int dd;
 } Date;
+
 typedef struct
 {
     char username[MAX_SIZE_USER_NAME];
     char password[MAX_SIZE_PASSWORD];
 } sFileHeader;
+
 typedef struct// to call in program
 {
     unsigned int books_id; // declare the integer data type
