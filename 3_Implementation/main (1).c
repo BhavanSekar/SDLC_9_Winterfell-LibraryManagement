c3a97f02ac38e2004c1cefea28d3ea9f452a19dd
Megumi-anushka <anushka.singru9@gmail.com>
2021-08-22 11:33:51 +0530

    bool function and main file

---
+++ 3_Implementation/main (1).c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <ctype.h>
+#include <string.h>
+#include "var.c"
+int main()
+{
+    init();
+    welcomeMessage();
+    login();
+    return 0;
+}
