bfda469f0b793e7ab1d426d8ff0d52334cd22a7a
Megumi-anushka <anushka.singru9@gmail.com>
2021-08-24 17:00:51 +0530

    update unittesting

---
+++ 3_Implementation/UnitTesting/var1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <ctype.h>
+#include <string.h>
+
+char test_book[20]="Bhavan";
+
+char test_author[20]="Bhavan";
+
+int test_pages=23;
+
+int test_price=24;
+
+char test_book1[20]="Rahul";
+
+char test_author1[20]="Rahul";
+
+int test_pages1=22;
+
+int test_price1=23;
+
+char test_book2[20]="Madhu";
+
+char test_author2[20]="Madhu";
+
+int test_pages2=21;
+
+int test_price2=22;
+
